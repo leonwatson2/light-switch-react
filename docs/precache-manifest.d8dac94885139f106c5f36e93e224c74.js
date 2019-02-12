0152edbd53505984cab329f114ffc97042d7eea2
vlw0052 <leon@vlw2.com>
2019-02-12 09:56:48 -0600

    Compiled App

---
+++ docs/precache-manifest.d8dac94885139f106c5f36e93e224c74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c8eb1b8ab055f3288492",
+    "url": "/static/js/main.c8eb1b8a.chunk.js"
+  },
+  {
+    "revision": "8fef2fe04b27ce3e94b0",
+    "url": "/static/js/2.8fef2fe0.chunk.js"
+  },
+  {
+    "revision": "c8eb1b8ab055f3288492",
+    "url": "/static/css/main.6643f59f.chunk.css"
+  },
+  {
+    "revision": "bdcfb9e8b63366248a5e1df900295de0",
+    "url": "/index.html"
+  }
+];
